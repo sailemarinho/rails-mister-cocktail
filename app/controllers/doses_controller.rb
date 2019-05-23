6edb11a2c2f231ca3e4473b15f8831c4c01743ff
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-05-23 14:54:52 -0300

    Add controllers and views for cocktails and doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_url, notice: 'Cocktail was successfully destroyed.'
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses, only: %i[index show destroy]
+  resources :cocktails do
+    resources :doses, only: %i[new create edit update]
+  end
 end
