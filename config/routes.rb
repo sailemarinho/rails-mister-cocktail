afb170498e70d94cf61fbddab4686d20288f029b
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-05-24 15:34:11 -0300

    Root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :doses, only: %i[index show destroy]
   resources :cocktails do
     resources :doses, only: %i[new create edit update]
